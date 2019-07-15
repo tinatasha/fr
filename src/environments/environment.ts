c1acd2f287231c8e929d62545490767bd09f738c
tinatasha <tashambiti@gmail.com>
2019-07-15 14:44:42 +0300

    fist commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: "https://api.github.com/users/",
+  apikey: "?access_token=0ff610d62888f906dbf5c067b0c783b468b7462f",
+  apiRepokey: "/repos?access_token=0ff610d62888f906dbf5c067b0c783b468b7462f"
+};
